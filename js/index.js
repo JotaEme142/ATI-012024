186bdb4fd158d38799a549f261dd8b85b0695b5e
José Torres <jotaeme142@gmail.com>
2024-05-16 23:41:48 -0400

    cambio de lenguaje segun atributo (solo index)

--- js/index.js
+++ js/index.js
@@ -1,10 +1,35 @@
 window.onload = function () {
+    getParams();
     buildMenu();
     buildList();
+    buildProfile();
 };
 
+let configFilePath = "";
+let profilePath = "";
+
+function getParams(){
+    const parametrosURL = new URLSearchParams (window.location.search);
+    const idioma = parametrosURL.get('lang');
+    const perfil = parametrosURL.get('cedula');
+    console.log(idioma);
+    console.log(perfil);
+    if (idioma == null) {
+        configFilePath = `conf/configES.json`;
+    }else{
+        configFilePath = `conf/config${idioma.toUpperCase()}.json`;
+    }
+    if (perfil == null){
+        profilePath = `../23616754/perfil.json`;
+    }else{
+        profilePath = `../${perfil}/perfil.json`;
+        
+    }
+
+}
+
 function buildMenu() {
-    fetch("../conf/configES.json")
+    fetch(configFilePath)
         .then((respuesta) => respuesta.json())
         .then((CONF) => {
             let menu = document.getElementById("menu");
@@ -17,11 +42,16 @@ function buildMenu() {
             const menuSearch = document.createElement("li");
             menuSearch.innerHTML = `<a id="menuSearch" href="#">
             <form>
-                <input type="search" placeholder="Buscar" />
-                <button type="submit">Buscar</button>
+                <input type="search" placeholder="${CONF.buscar}" />
+                <button type="submit">${CONF.buscar}</button>
             </form></a>`;
             menu.appendChild(menuSearch);
+            let footer = document.getElementById("footer");
+            const copyRight = document.createElement("div");
+            copyRight.innerHTML = `<p> ${CONF.copyRight} </p>`;
+            footer.appendChild(copyRight);
         })
+        .catch((error) => console.log("Hubo un error: " + error.message));
 }
 
 function buildList() {
@@ -37,6 +67,20 @@ function buildList() {
                 list.appendChild(estudianteActual);
             });
         })
-        .catch((error) => console.log("Hubor un error: " + error.message));
+        .catch((error) => console.log("Hubo un error: " + error.message));
+}
+
+function buildProfile(){
+    fetch(profilePath)
+    .then((respuesta) => respuesta.json())
+    .then((PROFILE) => {
+        let profileContainer = document.getElementById("profile-container");
+        const photo = document.createElement("div");
+        photo.innerHTML = `<img id=""rounded-photo src="../${PROFILE.ci}/${PROFILE.ci}.*/>"`;
+        profileContainer.appendChild(photo);
+        let profileBox = document.getElementById("profile-box");
+
+    })
+    .catch((error) => console.log("Hubo un error: " + error.message));
 }
 
